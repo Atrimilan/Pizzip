7e3e98f5d1d1d3045c2a768a32f65288370b5e29
Atrimilan <milan.nicolas.auditeur@lecnam.net>
2021-04-28 11:28:53 +0200

    master

--- view/pages/Footer.php
+++ view/pages/Footer.php
@@ -2,14 +2,13 @@
 //  --  ---------------  FOOTER PRESENT SUR TOUTES LES PAGES  ---------------  -->
 ?>
 
-<link rel="stylesheet" href="../CSS/footer.css" media="screen">
+<link rel="stylesheet" href="../../model/style/style.css" media="screen">
 
-        <footer id="bas_page">
-                <p>
-                    <a href="https://google.com/" id="mail_PIZZIP" target="_blank" title="mail de Pizzip">info@pizzip.com</a> <br>
-                    <br> Numéro : 0302020202
-                    <br>1 rue du nom 71100 Chalon-Sur-Saône <br>
-                    <br>Vente en ligne de Pizza compréssée<br>Tous droits réservés<br>
-                </p>
-        </footer>
-
+<footer id="bas_page">
+        <p>
+                <a href="https://google.com/" id="mail_PIZZIP" target="_blank" title="mail de Pizzip">info@pizzip.com</a> <br>
+                <br> Numéro : 0302020202
+                <br>1 rue du nom 71100 Chalon-Sur-Saône <br>
+                <br>Vente en ligne de Pizza compréssée<br>Tous droits réservés<br>
+        </p>
+</footer>
