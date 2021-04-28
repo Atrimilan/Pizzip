2f79e825898f2626379f7ecf801cc02de127701c
flordin lountala <flordinlountala@gmail.com>
2021-04-28 11:14:03 +0200

    commit1

---
+++ view/pages/Footer.php
@@ -0,0 +1,15 @@
+<?php
+//  --  ---------------  FOOTER PRESENT SUR TOUTES LES PAGES  ---------------  -->
+?>
+
+<link rel="stylesheet" href="../CSS/footer.css" media="screen">
+
+        <footer id="bas_page">
+                <p>
+                    <a href="https://google.com/" id="mail_PIZZIP" target="_blank" title="mail de Pizzip">info@pizzip.com</a> <br>
+                    <br> Numéro : 0302020202
+                    <br>1 rue du nom 71100 Chalon-Sur-Saône <br>
+                    <br>Vente en ligne de Pizza compréssée<br>Tous droits réservés<br>
+                </p>
+        </footer>
+
