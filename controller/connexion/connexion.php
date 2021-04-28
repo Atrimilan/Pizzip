5ab146e75f8e9385abaff8673a9395d60f31f997
Kevin MUTH <muth.kevin@hotmail.fr>
2021-04-28 11:31:50 +0200

    connexion bdd

---
+++ controller/connexion/connexion.php
@@ -0,0 +1,12 @@
+<?php
+//Autor MUTH Kévin
+$host = "remotemysql.com";
+$user = "eEPfirnc1C";
+$pwd = "vZFMsKIeXw";
+$bdd = "eEPfirnc1C";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
