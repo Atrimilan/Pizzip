e562d98b8f77b4797d231f672bd0e322e335d5aa
Kevin MUTH <muth.kevin@hotmail.fr>
2021-04-28 11:32:29 +0200

    teste bdd kevin

--- controller/connexion/connexion.php
+++ controller/connexion/connexion.php
@@ -8,5 +8,5 @@ $bdd = "eEPfirnc1C";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor   
  */
