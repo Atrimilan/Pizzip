3a16cce5a2ba810cd0475b36696bf8787df3388a
Kevin MUTH <muth.kevin@hotmail.fr>
2021-04-28 11:39:21 +0200

    v1.0

--- controller/connexion/connexion.php
+++ controller/connexion/connexion.php
@@ -1,5 +1,5 @@
 <?php
-//Autor MUTH Kévin
+//Author MUTH Kévin
 $host = "remotemysql.com";
 $user = "eEPfirnc1C";
 $pwd = "vZFMsKIeXw";
@@ -8,5 +8,5 @@ $bdd = "eEPfirnc1C";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor km   
+ * and open the template in the editor   
  */
