9ac3f5451c79840d59cd46664230ea4773435b15
Kevin MUTH <muth.kevin@hotmail.fr>
2021-04-28 11:51:08 +0200

    v2.0

---
+++ view/pages/vue_GestionStocks.php
@@ -0,0 +1,58 @@
+<?php
+//CONNEXION A LA BASE DE DONNEE
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if (isset($_GET['fic'])){
+	$fic=$_GET['fic'];
+	require_once '../../controller/connexion.php';
+        if (!empty($_GET)) {
+            // CONNEXION PDO
+            try {
+                $connex = new PDO('mysql:host=' . $host . ';dbname=' . $bdd, $user, $pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            } catch (PDOException $e) {
+                echo 'Erreur : ' . $e->getMessage() . '<br />';
+                echo 'N° : ' . $e->getCode();
+                die();
+            }
+            try {
+                //REQUETE : JOINTURE ENTRE LA TABLE FOURS ET DATA
+            	$requete= $connex->query("SELECT data.date, data.heure, data.temperature, fours.four_marque "
+                        . "FROM data, fours WHERE data.four_id=".$fic." AND fours.four_id=".$fic);               
+            } catch (PDOException $e) {
+                print $e->getMessage();
+            }
+	
+	$maListe = new stdClass();
+	foreach($requete as $key => $value){
+            $maListe->{$key} = $value;
+	}
+	echo json_encode($maListe, JSON_FORCE_OBJECT);
+    }
+}
+?>
+
+
+<html>
+    <head>
+        <title>Pizzip - GESTIONNAIRE DES STOCKS</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="MUTH Kevin">        
+    </head>
+    
+    <body>
+        <div>
+            <h1><b>Pizzip - GESTIONNAIRE DES STOCKS</b></h1>
+        </div>
+        
+        <div>
+            
+        </div>
+    </body>
+</html>
+
+<?php
