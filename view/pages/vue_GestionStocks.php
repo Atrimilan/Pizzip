6e78008e8c2ba65ffec53f504d42fcbd472949e8
kev-sov <muth.kevin@hotmail.fr>
2021-04-30 10:57:13 +0200

    v1.5

--- view/pages/vue_GestionStocks.php
+++ view/pages/vue_GestionStocks.php
@@ -1,61 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title></title>
-     <script src="https://code.jquery.com/jquery-latest.js"></script>
-     <meta name="author" content="MUTH Kévin">
-</head>
-    <body>
-    <div id="stage" style="background-color:#eee;">&nbsp;</div>
-         <form id="civilite">
-            <label for="mesures-select">Liste des fours :</label>
-            <select name="mesures" id="mesures-select">
-                <?php
-                require_once '../PizzipProjet/view/pages/connexion.php';
-                    // CONNEXION PDO
-                    try {
-                        $connex = new PDO('mysql:host=' . $host . ';dbname=' . $bdd, $user, $pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                    } catch (PDOException $e) {
-                        echo 'Erreur : ' . $e->getMessage() . '<br />';
-                        echo 'N° : ' . $e->getCode();
-                        die();
-                    }
-                    try {
-                        //REQUETE
-                        $requete =$connex->query("SELECT* INGREDIENT");               
-                    } catch (PDOException $e) {
-                        print $e->getMessage();
-                    }
-                    while ($ingre =$requete->fetch(PDO::FETCH_ASSOC)){ //FETCH POUR RECUPERER LES DONNEES
-                        echo "<option value=".$ingre['NomIngred'].">".$ingre['Frais']." || ".$ingre['Unite'].$ingre['StockMin']
-                        .$ingre['StockReel'].$ingre['PrixUHT_Moyen'].$ingre['Q_A_Com'].$ingre['DateArchiv']."</option>";
-                    }
-                ?>
-            </select>
-        </form>
-    
-    <div id="mesures">
-        <h1 id=t_mes> Liste ingrédients </h1>
-        <h1 id=t_fic> </h1>
-        <ul> 
+<?php
+$host = "remotemysql.com";
+$user = "eEPfirnc1C";
+$pwd = "vZFMsKIeXw";
+$bdd = "eEPfirnc1C";
 
-        </ul>
-    </div>    
-        <script type="text/javascript"> 
-              $(document).ready(function(){ 
-                    $("#mesures-select").change(function () {
-                        $('ul').empty();
-                        var selec=$('#mesures-select option:selected').val();
-                        selec='fic='+selec.trim();
-                        $.getJSON( "lecture.php", selec, function(mess) {
-                            $.each(mess, function(key, val){
-                               $('ul').append("<li> Marque : "+val["four_marque"]+" - Date : "+val["date"]+" - Heure : "+val["heure"]+
-                                       " - Temperature :"+val["temperature"]+"°C </li>"); 
-                            })                            
-                        });
-                    }); 
-              });
-        </script>  
-    </body>
+try {
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $bdd, $user, $pwd);
+} catch (PDOException $e) {
+    echo 'Erreur : ' . $e->getMessage() . '<br />';
+    echo 'NÂ° : ' . $e->getCode();
+    die();
+}
+?>
+<html>
+    <h1>IHM Gérant</h1>
+    <sectiond id="ingredient">
+        <h2>Ingrédient</h2>        
+        <?php
+            try {
+                $result = $pdo->query("SELECT * FROM INGREDIENT");    // SQL par PDO
+
+                while ($tabIngre = $result->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<li>".$tabIngre['NomIngred']." | ".$tabIngre['Unite']." | ".$tabIngre['StockMin']." | "
+                    .$tabIngre['StockReel']." | ".$tabIngre['PrixUHT_Moyen']." | ".$tabIngre['Q_A_Com']
+                    ." | ".$tabIngre['DateArchiv']."</li>";
+                }
+                } catch (PDOException $e) {
+                print $e->getMessage();
+            }
+        ?>
+    </section>
 </html>
