10e95f670ee8cf7da9e077bfb768cecfe581f9da
kev-sov <muth.kevin@hotmail.fr>
2021-04-30 09:53:46 +0200

    v1.4

--- view/pages/vue_GestionStocks.php
+++ view/pages/vue_GestionStocks.php
@@ -1,50 +1,61 @@
-<?php
-//CONNEXION A LA BASE DE DONNEE
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-if (isset($_GET['fic'])){
-	$fic=$_GET['fic'];
-	require_once 'connexion.php'; // ../../controller/connexion.php
-        if (!empty($_GET)) {
-            // CONNEXION PDO
-            try {
-                $connex = new PDO('mysql:host=' . $host . ';dbname=' . $bdd, $user, $pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            } catch (PDOException $e) {
-                echo 'Erreur : ' . $e->getMessage() . '<br />';
-                echo 'N° : ' . $e->getCode();
-                die();
-            }
-            try {
-                //REQUETE : JOINTURE ENTRE LA TABLE FOURS ET DATA
-            	/*$requete= $connex->query("SELECT data.date, data.heure, data.temperature, fours.four_marque "
-                        . "FROM data, fours WHERE data.four_id=".$fic." AND fours.four_id=".$fic);      */  
-                $requete= $connex->query("select* from INGREDIENT".$fic);       
-            } catch (PDOException $e) {
-                print $e->getMessage();
-            }
-}
-?>
-
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Pizzip - GESTIONNAIRE DES STOCKS</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="MUTH Kevin">        
-    </head>
-    
+<head>
+<meta charset="utf-8" />
+<title></title>
+     <script src="https://code.jquery.com/jquery-latest.js"></script>
+     <meta name="author" content="MUTH Kévin">
+</head>
     <body>
-        <div>
-            <h1><b>Pizzip - GESTIONNAIRE DES STOCKS</b></h1>
-        </div>
-        
-        <div>
-            
-        </div>
+    <div id="stage" style="background-color:#eee;">&nbsp;</div>
+         <form id="civilite">
+            <label for="mesures-select">Liste des fours :</label>
+            <select name="mesures" id="mesures-select">
+                <?php
+                require_once '../PizzipProjet/view/pages/connexion.php';
+                    // CONNEXION PDO
+                    try {
+                        $connex = new PDO('mysql:host=' . $host . ';dbname=' . $bdd, $user, $pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                    } catch (PDOException $e) {
+                        echo 'Erreur : ' . $e->getMessage() . '<br />';
+                        echo 'N° : ' . $e->getCode();
+                        die();
+                    }
+                    try {
+                        //REQUETE
+                        $requete =$connex->query("SELECT* INGREDIENT");               
+                    } catch (PDOException $e) {
+                        print $e->getMessage();
+                    }
+                    while ($four =$requete->fetch(PDO::FETCH_ASSOC)){ //FETCH POUR RECUPERER LES DONNEES
+                        echo "<option value=".$four['NomIngred'].">".$four['Frais']." || ".$four['Unite'].$four['StockMin']
+                        .$four['StockReel'].$four['PrixUHT_Moyen'].$four['Q_A_Com'].$four['DateArchiv']."</option>";
+                    }
+                ?>
+            </select>
+        </form>
+    
+    <div id="mesures">
+        <h1 id=t_mes> Liste de mesures </h1>
+        <h1 id=t_fic> </h1>
+        <ul> 
+
+        </ul>
+    </div>    
+        <script type="text/javascript"> 
+              $(document).ready(function(){ 
+                    $("#mesures-select").change(function () {
+                        $('ul').empty();
+                        var selec=$('#mesures-select option:selected').val();
+                        selec='fic='+selec.trim();
+                        $.getJSON( "lecture.php", selec, function(mess) {
+                            $.each(mess, function(key, val){
+                               $('ul').append("<li> Marque : "+val["four_marque"]+" - Date : "+val["date"]+" - Heure : "+val["heure"]+
+                                       " - Temperature :"+val["temperature"]+"°C </li>"); 
+                            })                            
+                        });
+                    }); 
+              });
+        </script>  
     </body>
 </html>
