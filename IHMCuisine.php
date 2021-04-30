e38329b914990479370b788e83025bc59d15ada9
julesMizu <jules.kantzer.auditeur@lecnam.net>
2021-04-30 09:51:17 +0200

    premier commit

---
+++ IHMCuisine.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="IHMcuisine" content="IHMcuisine">
+    </head>
+    <body>
+        <div>Commande à préparer</div>
+        <button id="rafraichir">actualiser</button>
+    </body>
+    <script type="text/javascript">
+            $(document).ready(function () {
+                $("#rafraichir").click(function () {
+                   
+                    })
+                });
+        </script>  
+</html>
