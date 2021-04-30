905f458791d3f87b67132d0755e55b2262a42f1e
kev-sov <muth.kevin@hotmail.fr>
2021-04-30 09:02:11 +0200

    connexion v1.0

---
+++ view/pages/connexion.php
@@ -0,0 +1,13 @@
+<?php
+<?php
+//Autor MUTH Kévin
+$host = "https://remotemysql.com/phpmyadmin/index.php";
+$user = "eEPfirnc1C";
+$pwd = "vZFMsKIeXw";
+$bdd = "eEPfirnc1C";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
