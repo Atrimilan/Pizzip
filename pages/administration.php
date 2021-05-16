fe295278387e727499c86bad426c7da103264126
kev-sov <muth.kevin@hotmail.fr>
2021-05-16 18:57:25 +0200

    16-05-2021 18:57

--- pages/administration.php
+++ pages/administration.php
@@ -1,4 +1,5 @@
 <html>
+<link rel="stylesheet" href="../model/style.css"/>
 <title>Piz.zip - Administration</title>
 <h1>IHM Administration</h1>
 <section id="ingredient">
@@ -8,17 +9,22 @@
     try {
         $result = $pdo->query("SELECT * FROM INGREDIENT");    // SQL par PDO
 
+        /* ------ AFFICHAGE DES INGRENDIENTS -------*/
+        $i=1;
         while ($tabIngre = $result->fetch(PDO::FETCH_ASSOC)) {
-            echo "<li>" . $tabIngre['NomIngred'] . " | " . $tabIngre['Unite'] . " | " . $tabIngre['StockMin'] . " | "
-                . $tabIngre['StockReel'] . " | " . $tabIngre['PrixUHT_Moyen'] . " | " . $tabIngre['Q_A_Com']
-                . " | " . $tabIngre['DateArchiv'];
-            echo " <button>Selection</button> ";
-            if ($tabIngre['StockReel']<5){
-                echo "<b>A recharger</b> </li>";
+            echo "<li>" . $tabIngre['NomIngred']; /*. " | Qte alerte : " . $tabIngre['StockMin'] . " | Qte réel :"
+                . $tabIngre['StockReel'] . " | Prix UHT :" . $tabIngre['PrixUHT_Moyen'] . " | " . $tabIngre['Q_A_Com']
+                . " | " . $tabIngre['DateArchiv'];*/
+            echo " <button class='buttonIng'"." id='buttonIng".$i."'"."> Selectionner </button> ";
+            if ($tabIngre['StockReel']<=$tabIngre['StockMin']){
+                echo "<b> Prévoir réapprovisionnement </b> </li>";
             }
-            echo "</li>";
-        }
-    } catch (PDOException $e) {
+            echo " </li>";
+            $i++; // incrémentation pour de l'id boutton             
+        } // (fin while)
+        echo "<button class='ajouterIng' id='ajouterIng'> Ajouter Ingrédient </button>";
+    } 
+    catch (PDOException $e) {
         print $e->getMessage();
     }
     ?>
--- pages/ingredient_ajout.php
+++ pages/ingredient_ajout.php
@@ -1,3 +1,13 @@
-<?php
-// PAGE INGREDIENT : AJOUT DE QUANTITE
+<html>
+<title>Piz.zip - Administration</title>
+<h1>IHM Administration</h1>
+<section id="ingredient">
+    <h2>Ingrédient - ajouter</h2>
+    <?php
+    $ingredient=$_GET[''];
+    require_once("../pages/administration.php");
+    if (){};
+    ?>
+</section>
 
+</html>
