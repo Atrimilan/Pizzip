80ef613342e0e53510e1f8290dffe562e750e449
kev-sov <muth.kevin@hotmail.fr>
2021-05-16 23:59:29 +0200

    16-05-2021 23-59

--- pages/ajouter_ing.php
+++ pages/ajouter_ing.php
@@ -2,14 +2,27 @@
     <?php
     require_once("../controller/connexion.php");
     $idIngred=$_POST['IdIngred'];
-    $frais=$_POST['NomIngred'];
     $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
-    $NomIngred=$_POST['NomIngred'];
+    $frais=$_POST['frais'];
+    $unite=$_POST['unite'];
+    $StockMin=$_POST['StockMin'];
+    $StockReel=$_POST['StockReel'];
+    $PrixUHT=$_POST['PrixUHT_Moyen'];
+    $Q_A_Com=$_POST['Q_A_Com'];
+    $DateArchiv=$_POST['DateArchiv'];
+
+    // ------------------------ AJOUT INGREDIENT ------------------------------
+    if (isset($_POST['ajout'])){ //condition pour vérifié que les champs sont remplis
+        if (empty($idIngred) || empty($NomIngred) || empty($StockMin) ||
+        empty($StockReel) || empty($PrixUHT) || empty($Q_A_Com) || empty($DateArchiv)){
+            echo "<p style='color:red'>veuillez compléter svp.</p>";
+        }
+        echo "bien remplis : very good";
+    };
+
+    $req=$bdd->query("INSERT INTO 'INGREDIENT' ('IdIngred', 'NomIngred', 'Frais', 'Unite', 'StockMin',
+                                    'StockReel', 'PrixUHT_Moyen', 'Q_A_Com', 'DateArchiv')"
+                                    ." VALUES (".$idIngred.",".$NomIngred.",".$frais.",".$unite.",".$StockMin.
+                                    ",".$StockReel.",".$PrixUHT.",".$Q_A_Com.",".$DateArchiv.")");
     ?>
 </html>
