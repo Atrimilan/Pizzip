9b98446f10799b937b967df2efeca7f79ebd8689
julesMizu <65505182+julesMizu@users.noreply.github.com>
2021-05-21 14:08:14 +0200

    test Html

--- PizzipModif/vue/IHMadmin.php
+++ PizzipModif/vue/IHMadmin.php
@@ -6,23 +6,70 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Cuisine</title>
+        <title>Admin</title>
         <meta charset="UTF-8">
         <meta name="IHMcuisine" content="IHMcuisine">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/bootstrap.css">
 
     </head>
     <body>
         <div class="container">
+            <h1>Modification </h1>    
+            <table class="table table-bordered ", style="margin-top:15px;">
+                <thead class="thead-dark">
+                    <tr><th class="col-md-1">Nom Ingredient</th><th class="col-md-4">Option</th><th class="col-md-2">actions</th></tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tomate</td>
+                        <td>   
+                            <div>
+                                <span>est frais</span>
+                                <input type="checkbox" checked data-toggle="toggle">
+                            </div>
 
+                            <div>
+                                <span>Unité : </span>
+                                <select name="unite" id="unite">
+                                    <option value="">--choisir unite--</option>
+                                    <option value="kg">kilogramme</option>
+                                    <option value="litre">Litre</opt>
+                                </select>
+                            </div>
+                            <div>
+                                <p>Stock min : </p>
+                                <input type="number" required name="price" min="0" value="0" >
+                            </div>
+                            <div>
+                                <p>Stock Réel :</p>
+                                <input type="number" required name="price" min="0" value="0" >
+                            </div>
+                            <div>
+                                <p>PUHT : </p>
+                                <input type="number" required name="price" min="0" value="0">
+                            </div>
+                            <div>
+                                <p>Quantité à commander : </p>
+                                <input type="number" required name="price" min="0" value="0">
+                            </div>
+
+
+
+
+                        </td>
+                        <td>
+                            <button class=" btn btn-success">ajouter</button>
+                            <button class=" btn btn-danger">supprimer</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <!-- comment -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src=" js/bootstrap.min.js"></script>
         <script type="text/javascript" src="../Controller/IHMadminJS.js"></script>
     </body>
 </html>
