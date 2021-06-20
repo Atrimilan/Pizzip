32fe123bb7542a6b06a6b871468f9f7f6e6ad5c2
Flordin Lountala <flordinlountala@gmail.com>
2021-06-20 03:58:10 +0200

    Update chargerTouteLivraison.php

--- controller/livreur/chargerTouteLivraison.php
+++ controller/livreur/chargerTouteLivraison.php
@@ -16,28 +16,11 @@ where A_Livrer = 'O' AND (Etat = 'prete' or Etat = 'enLivraison')";
 $result = $pdo -> query($requeteLivraisonCommande);                      
 while ($tabCommande = $result -> fetch(PDO :: FETCH_ASSOC) ) {
 
-    /*$numcommande = $tabCommande['NumCom'];
-    $nomClient = $tabCommande['NomClient'];
-    $telephoneClient = $tabCommande['TelClient'];
-    $adresseClient = $tabCommande['AdrClient'];
-    $codePostal = $tabCommande['CP_Client'];
-    $VilleClient = $tabCommande['VilClient'];
-    $dateCommande = $tabCommande['Date'];
-    $HeureDispo = $tabCommande['HeureDispo'];
-    $emballage = $tabCommande['TypeEmbal'];
-    $prix = $tabCommande['CoutLiv'];
-    //$livreur = $tabCommande['idLivreur'];
-    //$archive = $tabCommande['DateArchiv'];
-    $etatCommande = $tabCommande['Etat'];
-    $numDetail = $tabCommande['Num_Detail'];
-    $quantite = $tabCommande['Quant'];
-    $nomPizza = $tabCommande['NomPizza'];*/
-
     $tabComplet[] = $tabCommande;
               
 }
 
-$tableauJsonCommande = json_encode($tabComplet);
+$tableauJsonCommande = json_encode($tabComplet); // encode le tableu en JSON
 
 echo $tableauJsonCommande;
 
